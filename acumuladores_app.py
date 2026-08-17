d6dd4b17756109dcf19af08619b1e1ab17ec5f13
Arthur Rosin <66092745+ArthurRosin@users.noreply.github.com>
2026-08-17 11:14:29 -0300

    Update acumuladores_app.py
    Ajuste acc FX

--- acumuladores_app.py
+++ acumuladores_app.py
@@ -251,7 +251,7 @@ def gerar_texto(
         if commodity == "FX":
             linhas.append(
                 f"- Se, em qualquer momento, a PTAX tocar {nivel_ko} {u}, o acumulador desmonta. "
-                f"O montante {op_flex} a {nivel_melhorado} {u} permanecem."
+                f"O montante já acumulado a {nivel_melhorado} {u} permanece."
             )
         else:
             lotes_restantes = (
